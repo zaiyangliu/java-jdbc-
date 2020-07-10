73acbaf65ace885529bad8b0d39ad740e2b7c609
zaiyangliu <lzyxn@126.com>
2020-07-10 09:13:25 +0800

    update employeesForQuery1

--- src/com/atguigu3/preparedstatement/crud/EmployeesForQuery.java
+++ src/com/atguigu3/preparedstatement/crud/EmployeesForQuery.java
@@ -1,5 +1,28 @@
 package com.atguigu3.preparedstatement.crud;
 
-public class EmployeesForQuery {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import org.junit.Test;
+
+import com.atguigu3.util.JDBCUtils;
+
+/**
+ * @针对于employees表的查询操作
+ * @author tys10
+ *
+ */
+public class EmployeesForQuery {
+	@Test
+	public void testQuery1() throws Exception {
+		Connection conn = JDBCUtils.getConnection();
+		String sql = "select employee_id, first_name, last_name, email, phone_number, job_id, salary, commission_pct, manager_id, hiredate from employees where employee_id = ?";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		//执行并返回结果集
+		ResultSet resultSet = ps.executeQuery();
+		//处理结果集
+		
+		
+	}
 }
